bea3ef72da54506610ffab860ecec9cf76864c8e
parthenos0908 <distortion0908@gmail.com>
2021-02-10 07:28:47 +0900

    add word2vec trial

--- trial/tfidf.py
+++ trial/tfidf.py
@@ -6,14 +6,14 @@ import pandas as pd
 tfidf = TfidfVectorizer(stop_words="english")
 
 docs = np.array(["Human machine interface for lab abc computer applications",
-                "A survey of user opinion of computer system response time",
-                "The EPS user interface management system",
-                "System and human system engineering testing of EPS",              
-                "Relation of user perceived response time to error measurement",
-                "The generation of random binary unordered trees",
-                "The intersection graph of paths in trees",
-                "Graph minors IV Widths of trees and well quasi ordering",
-                "Graph minors A survey"])
+                 "A survey of user opinion of computer system response time",
+                 "The EPS user interface management system",
+                 "System and human system engineering testing of EPS",
+                 "Relation of user perceived response time to error measurement",
+                 "The generation of random binary unordered trees",
+                 "The intersection graph of paths in trees",
+                 "Graph minors IV Widths of trees and well quasi ordering",
+                 "Graph minors A survey"])
 
 # ベクトル化
 vec = tfidf.fit_transform(docs)
