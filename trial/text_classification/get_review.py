d8ed233066bbaa5ce05f49746d4602d833710f79
parthenos0908 <distortion0908@gmail.com>
2021-05-31 05:28:39 +0900

    add review data[D[D[D[_

--- trial/text_classification/get_review.py
+++ trial/text_classification/get_review.py
@@ -2,14 +2,15 @@ import requests
 import json
 from os import path
 
-game_id = 945360
+game_id = 427520
 url = "https://store.steampowered.com/appreviews/" + str(game_id) + "?json=1"
 params = {
-    "start_offset":0
-    }
+    "start_offset": 0
+}
 
 r = requests.get(url, params=params)
 
-json_filename = path.join(path.dirname(__file__), "reviewData/" + str(game_id) + ".json")
+json_filename = path.join(path.dirname(
+    __file__), "reviewData/" + str(game_id) + ".json")
 with open(json_filename, mode='w') as f:
     json.dump(r.json(), f, sort_keys=True, indent=4)
