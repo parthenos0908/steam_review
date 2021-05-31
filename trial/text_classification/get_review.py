977e35c71038badb89dd0ae163091e0b5080f99a
parthenos0908 <parthenos0908@i.softbank.jp>
2021-05-31 20:01:58 +0900

    edit jsongetter

--- trial/text_classification/get_review.py
+++ trial/text_classification/get_review.py
@@ -2,13 +2,24 @@ import requests
 import json
 from os import path
 
-game_id = 427520
+game_id = 227300
 url = "https://store.steampowered.com/appreviews/" + str(game_id) + "?json=1"
 params = {
-    "start_offset": 0
+    "filter": "all",
+    "language" : "english",
+    "day_range" : 9223372036854775807, # n日前までのreviewを取得(bigintの最大値を入力)
+    "review_type" : "all",
+    "purchase_type" : "all",
+    "num_per_page" : 100,
+    "start_offset": 0,
+    "cursor" : "*"
 }
+# https://partner.steamgames.com/doc/store/getreviews?l=japanese
 
 r = requests.get(url, params=params)
+
+cursor = r.json()["cursor"]
+print(cursor)
 
 json_filename = path.join(path.dirname(
     __file__), "reviewData/" + str(game_id) + ".json")
