a6f9ca65f27e0ce800a3f6a925eb0a8834d096dc
parthenos0908 <distortion0908@gmail.com>
2021-09-18 03:36:42 +0900

    labeled

--- code/labeling/filtering.py
+++ code/labeling/filtering.py
@@ -2,29 +2,49 @@ import enum
 import json
 from os import path
 
-INPUT_FILENAME = "f_in.json"
+INPUT_FILENAME = "review/255710_review_cleaned.json"
 OUTPUT_FILENAME = "f_out.json"
 
-BR_WORDS = ["bug", "crash", "fix"]
-FR_WORDS = [""]
+# BR_WORDS = ["bug", "fix", "problem", "issue", "defect", "crash", "solve"]
+BR_WORDS = ["bug", "fix", "crash"]
+# FR_WORDS = ["add", "please", "could", "would", "hope", "improve", "miss",
+#             "need", "prefer", "request", "should", "suggest", "want", "wish"]
+FR_WORDS = ["please", "hope", "improve", "need",
+            "prefer", "request", "suggest", "wish"]
 
 
 def main():
     input_filepath = path.join(path.dirname(__file__), INPUT_FILENAME)
     data = load_json(input_filepath)
 
-    BR_review = []
+    count_filtered_review(data)
+    filtered_review = filter_review(data)
+    print(len(filtered_review))
+
+    output_filepath = path.join(path.dirname(__file__), OUTPUT_FILENAME)
+    save_json(filtered_review, output_filepath)
+
+
+def filter_review(data):
+    filtered_review = []
     for i, datum in enumerate(data):
         flag = False
-        for br_word in BR_WORDS:
-            if br_word in datum["review_lem"]:
+        for fr_word in FR_WORDS:
+            if fr_word in datum["review_lem"]:
                 flag = True
         if flag:
-            BR_review.append(datum)
+            filtered_review.append(datum)
+    return filtered_review
 
-    print(len(BR_review))
-    output_filepath = path.join(path.dirname(__file__), OUTPUT_FILENAME)
-    save_json(BR_review, output_filepath)
+
+def count_filtered_review(data):
+    print("all data:{0}".format(len(data)))
+    for fr_word in FR_WORDS:
+        filtered_review = []
+        for i, datum in enumerate(data):
+            if fr_word in datum["review_lem"]:
+                filtered_review.append(datum)
+        print("{0}:{1}".format(fr_word, len(filtered_review)))
 
 
 def load_json(json_filepath):
