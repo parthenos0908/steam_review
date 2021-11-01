dc5ea6c9e58f70a4521f1daa682f172722d52fb1
parthenos0908 <parthenos0908@i.softbank.jp>
2021-11-02 08:17:26 +0900

    researched

--- code/research/research.py
+++ code/research/research.py
@@ -1,35 +1,84 @@
 import json
 from os import path
+import collections
+
+from scipy.sparse import data
+import seaborn as sns
 import matplotlib.pyplot as plt
 
+ID = [255710 ,227300, 427520]
 
-FORUM_FILENAME = "427520_forum_cleaned.json"
-REVIEW_FILENAME = "427520_review_cleaned_out.json"
+FORUM_FILENAMES = list(map(lambda x:"../../data/" + str(x) + "/" + str(x) + "_forum_cleaned.json", ID))
+REVIEW_FILENAMES = list(map(lambda x:"../../data/" + str(x) + "/" + str(x) + "_review_cleaned_out.json", ID))
 
+LABELS = ["review\n(Cities: Skylines)", "review\n(Euro Truck Sim)", "review\n(Factorio)", "forum\n(Cities: Skylines)", "forum\n(Euro Truck Sim)", "forum\n(Factorio)"]
+
+max_length = 128
 
 def main():
-    forum_filepath = path.join(path.dirname(__file__), FORUM_FILENAME)
-    forums = load_json(forum_filepath)
-    review_filepath = path.join(path.dirname(__file__), REVIEW_FILENAME)
-    reviews = load_json(review_filepath)
-    forum_num_words = []
-    for forum in forums:
-        forum_num_words.append(forum["num_words"])
-    review_num_words = []
-    for review in reviews:
-        review_num_words.append(review["num_words"])
+    forums = []
+    for f in FORUM_FILENAMES:
+        forums.append(load_json(path.join(path.dirname(__file__), f)))
+    reviews = []
+    for r in REVIEW_FILENAMES:
+        reviews.append(load_json(path.join(path.dirname(__file__), r)))
 
-    sum = 0
-    for num_words in forum_num_words:
-        sum += num_words
-    forum_average = sum / len(forum_num_words)
-    print(forum_average)
+    # 単語数の分布
+    forums_num_words = []
+    for i, forum in enumerate(forums):
+        tmp = []
+        sum = 0
+        for topics in forum:
+            tmp.append(topics["num_words"])
+            sum += topics["num_words"]
+        forums_num_words.append(tmp)
+        forum_average = sum / len(forum)
+        print("[forum{0}]:{1}".format(ID[i], forum_average))
 
-    sum = 0
-    for num_words in review_num_words:
-        sum += num_words
-    review_average = sum / len(review_num_words)
-    print(review_average)
+    reviews_num_words = []
+    for i, review in enumerate(reviews):
+        tmp = []
+        sum = 0
+        for topics in review:
+            tmp.append(topics["num_words"])
+            sum += topics["num_words"]
+        reviews_num_words.append(tmp)
+        review_average = sum / len(review)
+        print("[review{0}]:{1}".format(ID[i], review_average))
+
+    plt.figure(figsize=(10,6))
+    df = reviews_num_words + forums_num_words
+    plt.boxplot(df, labels=LABELS, sym="", showmeans=True)
+    plt.show()
+
+    # 頻出語彙
+    # plt.rcParams["font.size"] = 18
+    # for i, forum in enumerate(forums):
+    #     forum_word_list = []
+    #     for topics in forum:
+    #         if topics["num_words"] <= max_length:
+    #             forum_word_list.extend(topics["combined"].split())
+    #     c = collections.Counter(forum_word_list).most_common(20)
+    #     # print(c.most_common(20))
+    #     df = [val[0] for val in c]
+    #     label = [val[1] for val in c]
+    #     plt.title(LABELS[i], fontsize=16)
+    #     plt.barh(df[::-1], label[::-1])
+    #     plt.show()
+
+
+    # for i, review in enumerate(reviews):
+    #     review_word_list = []
+    #     for topics in review:
+    #         if topics["num_words"] <= max_length:
+    #             review_word_list.extend(topics["review_lem"].split())
+    #     c = collections.Counter(review_word_list).most_common(20)
+    #     # print(c.most_common(20))
+    #     df = [val[0] for val in c]
+    #     label = [val[1] for val in c]
+    #     plt.title(LABELS[i+3], fontsize=16)
+    #     plt.barh(df[::-1], label[::-1])
+    #     plt.show()
 
 
 def load_json(json_filepath):
